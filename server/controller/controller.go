2d66cb0eef905171fa7528b4cb5cdd197007c11b
fancyaction <labcoatdisaster@gmail.com>
2021-02-14 10:38:31 -0600

    Connect Cloudinary, add bird routes

--- server/controller/controller.go
+++ server/controller/controller.go
@@ -1,25 +1,59 @@
 package controller
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/komfy/cloudinary"
 )
+
+//Bird request from form data
+type Bird struct {
+	URL string `form:"url"`
+}
 
 // Pong tests that api is working
 func Pong(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"ping": "pong"})
 }
 
+// SendBirdImage uploads image to Cloudinary and then sends url to Flask API
+func SendBirdImage(c *gin.Context) {
+	cloudinaryURL := os.Getenv("CLOUDINARY_URL")
+	if cloudinaryURL == "" {
+		log.Fatalln("there is no env variable with given name")
+	}
+	cloudService, err := cloudinary.NewService(cloudinaryURL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fileHeader, _ := c.FormFile("file")
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	uploadResponse, err := cloudService.Upload(fileHeader.Filename, file, false)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"file":                  fileHeader.Filename,
+		"cloudinary_upload_url": uploadResponse.URL,
+	})
+}
+
+// SendBird receives url to send to Flask API
 func SendBird(c *gin.Context) {
-	// single file
-	file, _ := c.FormFile("file")
-	log.Println(file.Filename)
+	var bird Bird
+	c.Bind(&bird)
 
-	// Upload the file to specific dst.
-	// c.SaveUploadedFile(file, dst)
-
-	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+	c.JSON(200, gin.H{
+		"url": bird.URL,
+	})
 }
