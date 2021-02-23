47a768aee5a1eb251ff2c22b7317abdf5c884aaa
Allistair Everett <allistair.everett@fracing.com>
2021-02-23 13:08:33 -0600

    Disable env load

--- server/main.go
+++ server/main.go
@@ -1,20 +1,18 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/v27-bears-team-06/server/router"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
 	port := os.Getenv("PORT")
+	// port := "8080"
 
 	r := router.SetupRouter()
 	r.Run(port)
