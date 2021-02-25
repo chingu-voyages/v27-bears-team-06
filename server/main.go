d56d08c4381b6ecaa2c3a8b68ca4edae6723a349
Allistair Everett <allistair.everett@fracing.com>
2021-02-25 13:22:29 -0600

    Load env if env is local

--- server/main.go
+++ server/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/v27-bears-team-06/server/router"
 )
 
 func main() {
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	log.Fatal("Error loading .env file")
-	// }
-	// port := os.Getenv("PORT")
-	// port := "8080"
+	isLocal := os.Getenv("GO_ENV") == ""
+
+	if isLocal == true {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	port := os.Getenv("PORT")
 
 	r := router.SetupRouter()
-	r.Run()
+	r.Run(port)
 }
