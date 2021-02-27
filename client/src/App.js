7626a0726807fce549c8474b7e62891e8471af0c
fancyaction <labcoatdisaster@gmail.com>
2021-02-27 15:42:38 -0600

    Add ping test

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,21 @@ import About from './components/About';
 // import Map from './components/Map';
 import ImageContainer from './containers/ImageContainer';
 import './App.css';
-
+import axios from 'axios';
+import settings from './config/settings'
 
 //render UI components 
+
+const pingApi = async () => {
+    const response = await axios.get(`${settings.REACT_APP_API_URL}/ping`);
+    console.log('👀 ~ file: App.js ~ line 14 ~ pingApi ~ response', response);
+};
 
 function App() {
 	
 	return (
 		<>
+        <button onClick={pingApi}>PING</button>
 		<Header/>
 		<About/>
 		<ImageContainer/>
