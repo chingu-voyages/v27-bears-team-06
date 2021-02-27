b283e4feee23bc959b69a43d6f9b4b6e82343799
fancyaction <labcoatdisaster@gmail.com>
2021-02-27 15:08:34 -0600

    Test window._env_

--- client/src/containers/ImageContainer.js
+++ client/src/containers/ImageContainer.js
@@ -14,14 +14,14 @@ class ImageContainer extends React.Component{
     
   const formData = new FormData();
   formData.append('file', files[0]);
-  formData.append('upload_preset', process.env.REACT_APP_UPLOAD_PRESET);
+  formData.append('upload_preset', window._env_.REACT_APP_UPLOAD_PRESET);
 
   const response = {
   method: 'POST',
   body: formData,
   };
 
-  return fetch(`https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/image/upload`, response)
+  return fetch(`https://api.cloudinary.com/v1_1/${window._env_.REACT_APP_CLOUD_NAME}/image/upload`, response)
      
   .then(res => res.json())
   .then(res => {
@@ -36,15 +36,17 @@ class ImageContainer extends React.Component{
 //open the widget
 //open the widget
 openWidget = () => {
-    console.log('👀 ~ file: ImageContainer.js ~ line 44 ~ ImageContainer ~ process.env.REACT_APP_CLOUD_NAME', process.env.REACT_APP_CLOUD_NAME, process.env.REACT_APP_UPLOAD_PRESET, process.env.PORT);
+    console.log('👀 ~ file: ImageContainer.js ~ line 42 ~ ImageContainer ~ window._env_', window);
+    console.log('👀 ~ file: ImageContainer.js ~ line 42 ~ ImageContainer ~ window._env_', window._env_);
+    // console.log('👀 ~ file: ImageContainer.js ~ line 44 ~ ImageContainer ~ window._env_.REACT_APP_CLOUD_NAME', window._env_.REACT_APP_CLOUD_NAME, window._env_.REACT_APP_UPLOAD_PRESET, window._env_.PORT);
   window.cloudinary.createUploadWidget(
   {
-    cloudName: process.env.REACT_APP_CLOUD_NAME,
-    uploadPreset: process.env.REACT_APP_UPLOAD_PRESET,
+    cloudName: window._env_.REACT_APP_CLOUD_NAME,
+    uploadPreset: window._env_.REACT_APP_UPLOAD_PRESET,
   },	
   (error, { event, info }) => {
     if (event === 'success') {
-      //     fetch('https://api.cloudinary.com/v1_1/${process.env.REACT_APP_API_URL}/bird', {
+      //     fetch('https://api.cloudinary.com/v1_1/${window._env_.REACT_APP_API_URL}/bird', {
       //       method: 'POST',
       //       }
       //       .then(event => response.json())
