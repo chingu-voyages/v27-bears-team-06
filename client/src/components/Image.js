fd8f2035106430467d214017dc5d0f43d28f88ab
fancyaction <labcoatdisaster@gmail.com>
2021-02-28 10:26:26 -0600

    Add WidgetButton [RFR]

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-export default function image ({onInputChange,openWidget,imageUrl,imageAlt}) {
+const WidgetButton = ({openWidget}) => (
+    <button class="mt-12 px-8 py-4 rounded-l-xl rounded-t-xl ont-light antialiased tracking-wide bg-gradient-to-b from-blue-600 to-blue-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out"
+    onClick={openWidget}>
+    Upload Image Via Widget
+  </button>
+)
+
+export default function Image ({openWidget,imageUrl,imageAlt}) {
     
     return (
       <div>
         <div class="lg:2/6 xl:w-2/4 mt-4 lg:mt-40 lg:ml-16 text-left flex flex-col items-center">
-
-          <button class="mt-12 px-8 py-4 rounded-l-xl rounded-t-xl ont-light antialiased tracking-wide bg-gradient-to-b from-blue-600 to-blue-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out"
-            onClick={openWidget}>
-            Upload Image Via Widget
-          </button>
-
+        <WidgetButton openWidget={openWidget} />
         <div class="mt-12 lg:mt-32 lg:ml-20 text-left">
           <bottom type="button" class="flex items-center justify-center w-12 h-12 rounded-full bg-cool-gray-100 text-gray-800 animate-bounce hover:text-gray-900 hover:bg-cool-gray-50 transition duration-300 ease-in-out cursor-pointer">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
