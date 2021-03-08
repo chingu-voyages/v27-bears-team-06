bb2568ee7a6ccb396173dec37b0a2d64b8c0f0fe
fancyaction <labcoatdisaster@gmail.com>
2021-03-08 15:58:39 -0600

    Remove opacity, set to blue

--- client/src/components/Bird/Progress.js
+++ client/src/components/Bird/Progress.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Progress = (props) => {
     return (
-        <div className="h-8 w-screen flex justify-center top-0 left-0 bg-white opacity-75 z-50">
-            <span className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
+        <div className="h-8 flex justify-center top-0 left-0 bg-white z-50">
+            <span className="text-blue-500 top-1/2 my-0 mx-auto block relative w-0 h-0">
                 <i className="fas fa-circle-notch fa-spin fa-5x"></i>
            </span>
         </div>
