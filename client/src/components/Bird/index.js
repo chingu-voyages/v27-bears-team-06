19a6beb7732df9709a112eac5666b78669e4d5f7
fancyaction <labcoatdisaster@gmail.com>
2021-03-08 14:38:59 -0600

    Add Bird container component

---
+++ client/src/components/Bird/index.js
@@ -0,0 +1,62 @@
+import React, { useCallback, useEffect, useState, Fragment } from 'react';
+import BirdCard from './BirdCard';
+import WidgetButton from './WidgetButton';
+import axios from 'axios';
+import settings from 'config/settings';
+
+const mockedBirdData = {
+    id: 1,
+    image_url: 'https://cdn.britannica.com/57/76057-004-274A29D0.jpg',
+    name: 'Laysan Albatross',
+    species_info: {
+        synonyms: ['Diomedea immutabilis'],
+        otherCommonNames: ['Albatros de Laysan', 'Albatros de Laysan'],
+        kingdom: 'Animalia',
+        phylum: 'Craniata',
+        taxclass: 'Aves',
+        taxorder: 'Procellariiformes',
+        family: 'Diomedeidae',
+        genus: 'Phoebastria',
+        taxonomicComments:
+            'Formerly in genus <i>Diomedea</i>; tranferred to <i>Phoebastria </i>by AOU (1997). Occasionally hybridizes with <i>P. nigripes</i> (AOU 1998).',
+        informalTaxonomy: 'Animals - Vertebrates - Birds',
+    },
+};
+
+const Bird = () => {
+    const [birdDetails, setBirdDetails] = useState(mockedBirdData);
+    const [fileImage, setFileImage] = useState({ imageUrl: null, imageAlt: null });
+    const [loading, setLoading] = useState(false);
+
+    const onSuccess = useCallback((info) => {
+        setFileImage({ imageUrl: info.secure_url, imageAlt: `An image of ${info.original_filename}` });
+        setLoading(true);
+    }, []);
+
+    useEffect(() => {
+        const sendBirdData = () =>
+            axios
+                .get(`${settings.REACT_APP_API_URL}/bird`, { params: { image_url: fileImage.imageUrl } })
+                .then((response) => {
+                    setLoading(false);
+                    setBirdDetails(response.data);
+                    return response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+
+        if (fileImage.imageUrl) {
+            sendBirdData();
+        }
+    }, [fileImage.imageUrl]);
+
+    return (
+        <Fragment>
+            <WidgetButton onSuccess={onSuccess} loading={loading} />
+            <BirdCard loading={loading} birdDetails={birdDetails} {...fileImage} />
+        </Fragment>
+    );
+};
+
+export default Bird;
