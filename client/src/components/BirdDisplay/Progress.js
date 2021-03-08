ff40166585db824ff5fcdb134db713ed3db05dd1
fancyaction <labcoatdisaster@gmail.com>
2021-03-08 13:49:56 -0600

    Rename Loading to Progress

--- client/src/components/BirdDisplay/Progress.js
+++ client/src/components/BirdDisplay/Progress.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Loading = (props) => {
+const Progress = (props) => {
     return (
         <div className="h-8 w-screen flex justify-center top-0 left-0 bg-white opacity-75 z-50">
             <span className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
@@ -10,5 +10,5 @@ const Loading = (props) => {
     )
 }
 
-export default Loading;
+export default Progress;
 
