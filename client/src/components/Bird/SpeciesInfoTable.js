79b65e6ccd0a5c8c26a40404f6db372f13196923
fancyaction <labcoatdisaster@gmail.com>
2021-03-10 16:03:25 -0600

    Improve style in table

--- client/src/components/Bird/SpeciesInfoTable.js
+++ client/src/components/Bird/SpeciesInfoTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import useBreakpoints from 'utils/useBreakpoints';
 
 const SpecialInfoTable = ({
     synonyms,
@@ -10,65 +11,70 @@ const SpecialInfoTable = ({
     family,
     genus,
     informalTaxonomy,
-}) => (
-    <table class="table-auto shadow-lg mt-4">
-        <tbody>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Synonyms</strong>
-                </td>
-                <td class="border px-8 py-4">{synonyms}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Other Common Names</strong>
-                </td>
-                <td class="border px-8 py-4">{otherCommonNames}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Kingdom</strong>
-                </td>
-                <td class="border px-8 py-4">{kingdom}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Phylum</strong>
-                </td>
-                <td class="border px-8 py-4">{phylum}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Tax Class</strong>
-                </td>
-                <td class="border px-8 py-4">{taxclass}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Tax Order</strong>
-                </td>
-                <td class="border px-8 py-4">{taxorder}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Family</strong>
-                </td>
-                <td class="border px-8 py-4">{family}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Genus</strong>
-                </td>
-                <td class="border px-8 py-4">{genus}</td>
-            </tr>
-            <tr>
-                <td class="border px-8 py-4">
-                    <strong>Informal Taxonomy</strong>
-                </td>
-                <td class="border px-8 py-4">{informalTaxonomy}</td>
-            </tr>
-        </tbody>
-    </table>
-);
+}) => {
+    const { isXs, isSm } = useBreakpoints();
+    const cellStyle = isXs || isSm ? 'border px-2 py-1' : 'border px-8 py-4';
+
+    return (
+        <table className="table-auto shadow-lg mt-4">
+            <tbody>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Synonyms</strong>
+                    </td>
+                    <td className={cellStyle}>{synonyms}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Other Common Names</strong>
+                    </td>
+                    <td className={cellStyle}>{otherCommonNames}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Kingdom</strong>
+                    </td>
+                    <td className={cellStyle}>{kingdom}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Phylum</strong>
+                    </td>
+                    <td className={cellStyle}>{phylum}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Tax Class</strong>
+                    </td>
+                    <td className={cellStyle}>{taxclass}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Tax Order</strong>
+                    </td>
+                    <td className={cellStyle}>{taxorder}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Family</strong>
+                    </td>
+                    <td className={cellStyle}>{family}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Genus</strong>
+                    </td>
+                    <td className={cellStyle}>{genus}</td>
+                </tr>
+                <tr>
+                    <td className={cellStyle}>
+                        <strong>Informal Taxonomy</strong>
+                    </td>
+                    <td className={cellStyle}>{informalTaxonomy}</td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
 
 export default SpecialInfoTable;
