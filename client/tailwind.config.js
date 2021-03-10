c608cd2575be1ff9430211d6578a1c083a8e800f
fancyaction <labcoatdisaster@gmail.com>
2021-03-10 16:03:36 -0600

    Add more colors to tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        colors: {
+            // transparent: 'transparent',
+            current: 'currentColor',
+            ...colors
+          },
         extend: {
             fontFamily: {
                 'source-sans-pro': ['Source Sans Pro'],
