a75b13b74eadb4c898aaae0f474d58170bc2c205
fancyaction <labcoatdisaster@gmail.com>
2021-03-11 11:16:41 -0600

    Add mobile, desktop views

--- client/src/components/Bird/BirdCard.js
+++ client/src/components/Bird/BirdCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import parse from 'html-react-parser';
 import SpeciesInfoTable from './SpeciesInfoTable';
 import { useBreakpoints, Button, Card, CardContent } from 'utils';
@@ -9,50 +9,70 @@ const BirdImage = ({ src, alt }) => (
     </div>
 );
 
-const BirdDetails = ({ name, species_info: speciesInfo, imageUrl, imageAlt }) => {
+const MobileView = ({ record, imageUrl, imageAlt }) => {
     const [showMore, setShowMore] = useState(false);
+    const { name, species_info: speciesInfo } = record;
     const { taxonomicComments: description, ...otherInfo } = speciesInfo;
+
+    const Content = useCallback(
+        () =>
+            showMore ? (
+                <SpeciesInfoTable {...otherInfo} />
+            ) : (
+                <>
+                    <h1 className="text-2xl font-bold text-green-800 py-2">{name}</h1>
+                    <p className="mt-4 text-sm text-black break-all md:break-all ">{parse(description)}</p>
+                </>
+            ),
+        [description, name, otherInfo, showMore]
+    );
+
+    return (
+        <div className="p-2">
+            {imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}
+            <Content />
+            <Button onClick={() => setShowMore(!showMore)} label={showMore ? 'Hide Details' : 'Show Details'} />
+        </div>
+    );
+};
+
+const DesktopView = ({ record, imageUrl, imageAlt }) => {
+    const { name, species_info: speciesInfo } = record;
+    const { taxonomicComments: description, ...otherInfo } = speciesInfo;
+
+    return (
+        <>
+            <div className="p-4">
+                {imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}
+                <h1 className="text-2xl font-bold text-green-800 py-2">{name}</h1>
+                <p className="mt-4 text-sm text-black break-all md:break-all ">{parse(description)}</p>
+            </div>
+            <div className="p-4">
+                <SpeciesInfoTable {...otherInfo} />
+            </div>
+        </>
+    );
+};
+
+const BirdDetails = (props) => {
     const { isXs, isSm } = useBreakpoints();
     const isMobile = isXs || isSm;
 
     if (isMobile) {
-        return showMore ? (
-            <div className="p-2">
-                {imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}
-                <SpeciesInfoTable {...otherInfo} />
-                <Button onClick={() => setShowMore(!showMore)} label={showMore ? 'Hide Details' : 'Show Details'} />
-            </div>
-        ) : (
-            <div className="p-2">
-                {imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}
-                <h1 className="text-2xl font-bold text-green-800 py-2">{name}</h1>
-                <p className="mt-4 text-sm text-black break-all md:break-all ">{parse(description)}</p>
-                <Button onClick={() => setShowMore(!showMore)} label={showMore ? 'Hide Details' : 'Show Details'} />
-            </div>
-        );
+        return <MobileView {...props} />;
     }
 
-    return (
-        <>
-            <div className="p-2">
-                {imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}
-                <h1 className="text-2xl font-bold text-green-800 py-2">{name}</h1>
-                <p className="mt-4 text-sm text-black break-all md:break-all ">{parse(description)}</p>
-                <Button onClick={() => setShowMore(!showMore)} label={showMore ? 'Hide Details' : 'Show Details'} />
-            </div>
-            <div className="p-2">{showMore && <SpeciesInfoTable {...otherInfo} />}</div>
-        </>
-    );
+    return <DesktopView {...props} />;
 };
 
 export default function BirdCard({ imageUrl, imageAlt = 'Photo of bird', loading, record }) {
     return (
         <Card color="bg-teal-400">
-            <CardContent image={imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}>
+            <CardContent>
                 {!record || loading ? (
                     <span>Loading...</span>
                 ) : (
-                    <BirdDetails imageUrl={imageUrl} imageAlt={imageAlt} {...record} />
+                    <BirdDetails imageUrl={imageUrl} imageAlt={imageAlt} record={record} />
                 )}
             </CardContent>
         </Card>
