f6444656e5c0a6b85ff93ba28ca09dd4afc0fc2f
fancyaction <labcoatdisaster@gmail.com>
2021-03-11 17:25:39 -0600

    Add new header

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import Landing from 'components/Landing';
+import Header from 'components/Header';
+import About from 'components/About';
+import Bird from 'components/Bird';
+import Navbar from 'components/Navbar';
+import Footer from 'components/Footer';
 import './App.css';
 
 function App() {
     return (
-        <main className="overflow-y-scroll snap snap-y snap-proximity h-screen">
-            <Header />
-            <About />
-            <Bird />
-        </main>
+        <>
+            <Navbar transparent />
+            <main className="overflow-y-scroll snap snap-y snap-proximity h-screen">
+                <Header />
+                <About />
+                <Bird />
+     <Footer />     
+            </main>
+
+        </>
     );
 }
 export default App;
