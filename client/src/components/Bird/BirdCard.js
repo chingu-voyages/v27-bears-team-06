ed9b46e4b110ad9f299ecce212ab8479859a5b40
fancyaction <labcoatdisaster@gmail.com>
2021-03-11 10:40:56 -0600

    Update BirdCard style

--- client/src/components/Bird/BirdCard.js
+++ client/src/components/Bird/BirdCard.js
@@ -1,24 +1,11 @@
 import React, { useState } from 'react';
 import parse from 'html-react-parser';
 import SpeciesInfoTable from './SpeciesInfoTable';
-import Button from 'utils/Button';
-import useBreakpoints from 'utils/useBreakpoints';
+import { useBreakpoints, Button, Card, CardContent } from 'utils';
 
 const BirdImage = ({ src, alt }) => (
     <div className="flex justify-center">
         <img src={src} alt={alt} className="shadow-lg rounded max-w-full h-auto align-middle border-none" />
-    </div>
-);
-
-const Card = ({ children }) => (
-    <div className="p-24 bg-teal-400 flex justify-center">
-        <div className="md:max-w-md">{children}</div>
-    </div>
-);
-
-const CardContent = ({ children }) => (
-    <div className="flex align-bottom flex-col leading-none p-2 md:p-4 bg-white rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1">
-        <div className="flex flex-row justify-between items-center">{children}</div>
     </div>
 );
 
@@ -60,18 +47,14 @@ const BirdDetails = ({ name, species_info: speciesInfo, imageUrl, imageAlt }) =>
 
 export default function BirdCard({ imageUrl, imageAlt = 'Photo of bird', loading, record }) {
     return (
-        <div className="block">
-            <div className="flex items-center justify-center h-screen w-screen bg-teal-400">
-                <Card>
-                    <CardContent image={imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}>
-                        {!record || loading ? (
-                            <span>Loading...</span>
-                        ) : (
-                            <BirdDetails imageUrl={imageUrl} imageAlt={imageAlt} {...record} />
-                        )}
-                    </CardContent>
-                </Card>
-            </div>
-        </div>
+        <Card color="bg-teal-400">
+            <CardContent image={imageUrl && <BirdImage src={imageUrl} alt={imageAlt} />}>
+                {!record || loading ? (
+                    <span>Loading...</span>
+                ) : (
+                    <BirdDetails imageUrl={imageUrl} imageAlt={imageAlt} {...record} />
+                )}
+            </CardContent>
+        </Card>
     );
 }
