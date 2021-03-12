c797a74428909ae9b96164ac3ac142d88025097f
fancyaction <labcoatdisaster@gmail.com>
2021-03-12 12:04:37 -0600

    Refactor card

--- client/src/utils/Card.js
+++ client/src/utils/Card.js
@@ -1,11 +1,9 @@
 const Card = ({ children, color = 'bg-white' }) => (
-    <div className={`flex ${color} justify-center snap-center`}>
         <div
-            className={`${color} flex justify-center items-center h-screen w-screen`}
+            className={`${color} flex justify-center items-center h-screen snap-center`}
         >
             <div className="w-3/4 md:max-w-md">{children}</div>
         </div>
-    </div>
 );
 
 export default Card;
--- client/src/utils/CardContent.js
+++ client/src/utils/CardContent.js
@@ -1,6 +1,6 @@
 const CardContent = ({ children }) => (
-    <div className="flex align-bottom flex-col leading-none p-2 md:p-4 bg-white rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1">
-        <div className="flex flex-row justify-center items-center">{children}</div>
+    <div className="flex flex-col p-2 mb-1 mr-1 leading-none align-bottom bg-white rounded shadow outline-none md:p-4 hover:shadow-lg focus:outline-none">
+        <div className="flex flex-row items-center justify-center">{children}</div>
     </div>
 );
 
