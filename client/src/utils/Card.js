e1d381879b09866c372a150ab96dc50b5ac58b31
fancyaction <labcoatdisaster@gmail.com>
2021-03-14 14:35:13 -0500

    Add margin top to card

--- client/src/utils/Card.js
+++ client/src/utils/Card.js
@@ -1,11 +1,9 @@
 const Card = ({ children, color = 'bg-white' }) => (
-        <div
-            className={`${color} flex justify-center items-center h-screen snap-center`}
-        >
-            <div className="flex flex-col w-3/4 max-w-full md:max-w-md">
+    <div className={`${color} flex justify-center items-center h-screen snap-center`}>
+        <div className="flex flex-col w-3/4 max-w-full md:max-w-md" style={{ marginTop: '16.5vh' }}>
             {children}
-            </div>
         </div>
+    </div>
 );
 
 export default Card;
