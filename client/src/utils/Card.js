878abba480d8314a589867f56ac2df3adc4121be
fancyaction <labcoatdisaster@gmail.com>
2021-03-14 11:31:22 -0500

    Small card changes

--- client/src/utils/Card.js
+++ client/src/utils/Card.js
@@ -2,7 +2,9 @@ const Card = ({ children, color = 'bg-white' }) => (
         <div
             className={`${color} flex justify-center items-center h-screen snap-center`}
         >
-            <div className="w-3/4 md:max-w-md">{children}</div>
+            <div className="flex flex-col w-3/4 max-w-full md:max-w-md">
+            {children}
+            </div>
         </div>
 );
 
--- client/src/utils/CardContent.js
+++ client/src/utils/CardContent.js
@@ -1,5 +1,5 @@
 const CardContent = ({ children }) => (
-    <div className="flex flex-col p-2 mb-1 mr-1 leading-none align-bottom bg-white rounded shadow outline-none md:p-4 hover:shadow-lg focus:outline-none">
+    <div className="flex flex-col min-h-full p-2 mb-1 mr-1 leading-none align-bottom bg-white rounded shadow outline-none md:p-4 hover:shadow-lg focus:outline-none">
         <div className="flex flex-row items-center justify-center">{children}</div>
     </div>
 );
