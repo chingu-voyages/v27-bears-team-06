d9c54c3f75ec65fbea728755f2fe6ec1282b7f7e
fancyaction <labcoatdisaster@gmail.com>
2021-03-14 10:58:47 -0500

    Update GH link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 export default function Navigation() {
-  return (
-    <div
-      style={{
-        background: '#FFFFFF',
-      }}
-      className=" fixed top-0 w-screen z-40  "
-    >
-      <div className="text-3xl justify-between px-2 py-2 font-bold text-blue-500 flex-1 cursor-pointer flex items-center w-10/12 mx-auto py-3">
-          <h1 className="">
-            AVIAN   
-          </h1>
-          <a href="https://github.com/chingu-voyages/v27-bears-team-06/tree/develop"><i className="fab fa-github fa-lg"><span className= "text-lg ml-3">Github</span></i></a>
+    return (
+        <div
+            style={{
+                background: '#FFFFFF',
+            }}
+            className="fixed top-0 z-40 w-screen "
+        >
+            <div className="flex items-center justify-between flex-1 w-10/12 px-2 py-2 py-3 mx-auto text-3xl font-bold text-blue-500 cursor-pointer">
+                <h1 className="">AVIAN</h1>
+                <a href="https://github.com/chingu-voyages/v27-bears-team-06" rel="noreferrer" target="_blank">
+                    <i className="fab fa-github fa-lg">
+                        <span className="ml-3 text-lg">Github</span>
+                    </i>
+                </a>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
