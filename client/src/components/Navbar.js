ef3a7ca8d363ccdd3adfa20bd38cc58f83b75af4
fancyaction <labcoatdisaster@gmail.com>
2021-03-14 14:36:33 -0500

    Update navbar to fixed

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navigation() {
     return (
         <nav
-            className="relative z-40 w-full bg-white"
+            className="fixed z-40 w-full bg-white shadow-xl"
         >
             <div className="flex items-center justify-between flex-1 w-10/12 px-2 py-2 py-3 mx-auto text-3xl font-bold text-blue-500 cursor-pointer">
                 <h1 className="">AVIAN</h1>
